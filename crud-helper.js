cd8419fa85538f27bf83f8c42c732e5c1fbc2024
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-23 15:25:48 -0400

    Minor changes

--- crud-helper.js
+++ crud-helper.js
@@ -5,9 +5,9 @@ require('./config/database');
 
 // Require the Mongoose models
 const User = require('./models/User');
-// const Item = require('./models/item');
-// const Category = require('./models/category');
-// const Order = require('./models/order');
+const Item = require('./models/Items');
+const Category = require('./models/Category');
+const Order = require('./models/Order');
 
 let user, item, category, order;
 let users, items, categories, orders;
